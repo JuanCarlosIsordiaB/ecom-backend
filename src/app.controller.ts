5a5ef2b26c439527eef70bdbab29f78ca9055e09
JuanCarlosIsordiaB <juancarlosisordiab@gmail.com>
2025-12-06 17:19:18 -0600

    ecom backend built with NestJS

---
+++ src/app.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Post,Put } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('post-example')
+  getPost() {
+    return this.appService.getPostExample();
+  }
+
+
+  @Put('put-example')
+  getPut(){
+    return 'put example';
+  }
+}
+
