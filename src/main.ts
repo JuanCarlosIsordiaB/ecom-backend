c1db416b3a9ec9ac6e34d18f823d1c2be3ade507
JuanCarlosIsordiaB <juancarlosisordiab@gmail.com>
2025-12-22 22:03:06 -0600

    Static Images Configuration Done

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import 'reflect-metadata';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // it force to remove any properties that are not in the DTO
-  }));
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // it force to remove any properties that are not in the DTO
+    }),
+  );
+  app.useStaticAssets(join(__dirname, '../public'));
   //app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   await app.listen(process.env.PORT ?? 3000);
 }
